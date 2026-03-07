983544abafaef17be51763e8256c533392369a38
ragul-onemodo <ragul@onemodo.com>
2026-03-07 16:29:21 +0530

    first commit

--- database/migrations/2026_01_27_054715_create_trips_table.php
+++ database/migrations/2026_01_27_054715_create_trips_table.php
@@ -9,23 +9,36 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('trips', function (Blueprint $table) {
-            $table->id();
+
+            $table->id(); // BIGINT UNSIGNED PRIMARY KEY
             $table->uuid('trip_uuid')->unique();
-            $table->foreignId('device_id')->constrained('devices')->cascadeOnDelete();
-            $table->foreignId('entity_id')->constrained('entities')->cascadeOnDelete();
-            $table->string('vechicle_number')->nullable();
-            $table->string('direction'); // e.g., 'in' or 'out'
+
+            $table->foreignId('device_id')
+                ->constrained('devices')
+                ->cascadeOnDelete();
+
+            $table->foreignId('entity_id')
+                ->constrained('entities')
+                ->cascadeOnDelete();
+
+            $table->string('vehicle_number')->nullable();
+            $table->string('direction'); // in / out
+
             $table->string('snapshot');
             $table->string('top_image')->nullable();
+
             $table->string('device_ip')->nullable();
             $table->string('weight');
+
             $table->text('other_images')->nullable();
             $table->text('ocr_log')->nullable();
+
             $table->string('application_data')->nullable();
             $table->text('raw_data')->nullable();
 
             $table->timestamp('device_timestamp')->nullable();
-            $table->auditable();
+
+            $table->auditable(); // custom macro
         });
     }
 
